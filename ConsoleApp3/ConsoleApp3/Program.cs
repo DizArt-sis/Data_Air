54954705280913baad02628c9343c5ee67bf03fb
DizArt-sis <114420890+DizArt-sis@users.noreply.github.com>
2023-05-14 00:18:31 +0900

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string prompt = "Fly, but don't die";
+            //string[] options = { "Option 1", "Option 2", "Option 3" };
+            //Menu mainMenu = new Menu(prompt, options);
+            //int selectedIndex = mainMenu.Run();
+            //Console.ReadKey();
+
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+
+            Air newRess = new Air();
+            newRess.Start();
+
+        }
+    }
+}
